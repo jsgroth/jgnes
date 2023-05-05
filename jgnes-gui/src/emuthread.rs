cc6fc10da527a53a34a122f93999ca4868f407f9
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-05-05 06:06:49 -0500

    don't set panic hook in a loop

--- jgnes-gui/src/emuthread.rs
+++ jgnes-gui/src/emuthread.rs
@@ -12,28 +12,30 @@ pub(crate) fn start(
 ) -> Sender<JgnesNativeConfig> {
     let (sender, receiver) = mpsc::channel();
 
-    thread::spawn(move || loop {
+    thread::spawn(move || {
         // TODO maybe better way of reporting unsupported VSync mode errors than killing the process
         std::panic::set_hook(Box::new(|panic_info| {
             log::error!("Emulation thread panicked, killing process: {panic_info}");
             process::exit(1);
         }));
 
-        let config = match receiver.recv() {
-            Ok(config) => config,
-            Err(err) => {
-                log::info!("Emulation thread terminating due to recv error (most likely caused by closing main window): {err}");
-                return;
+        loop {
+            let config = match receiver.recv() {
+                Ok(config) => config,
+                Err(err) => {
+                    log::info!("Emulation thread terminating due to recv error (most likely caused by closing main window): {err}");
+                    return;
+                }
+            };
+
+            is_running.store(true, Ordering::Relaxed);
+            if let Err(err) = jgnes_native_driver::run(&config, dynamic_config.clone()) {
+                *emulation_error.lock().unwrap() = Some(err);
             }
-        };
 
-        is_running.store(true, Ordering::Relaxed);
-        if let Err(err) = jgnes_native_driver::run(&config, dynamic_config.clone()) {
-            *emulation_error.lock().unwrap() = Some(err);
+            is_running.store(false, Ordering::Relaxed);
+            dynamic_config.quit_signal.store(false, Ordering::Relaxed);
         }
-
-        is_running.store(false, Ordering::Relaxed);
-        dynamic_config.quit_signal.store(false, Ordering::Relaxed);
     });
 
     sender

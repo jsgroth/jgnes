9c38a16baecae4b66b5386a750663f81d9a2b0c3
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-05-23 21:12:41 -0500

    remove TextEncoder polyfill that is no longer necessary on wasm-bindgen 0.2.86

--- jgnes-web/js/audio-processor.js
+++ jgnes-web/js/audio-processor.js
@@ -1,9 +1,3 @@
-// Polyfill necessary because the generated wasm-bindgen JS requires TextEncoder and TextDecoder, but they are not
-// defined in the worklet thread.
-// This appears to have been fixed in wasm-bindgen 0.2.85 so that TextEncoder and TextDecoder are no longer required
-// to be defined, but due to transitive dependencies this project is currently locked at 0.2.84.
-import "./TextEncoder.js";
-
 import { initSync, AudioProcessor } from "../pkg/jgnes_web.js";
 
 class JgnesAudioProcessor extends AudioWorkletProcessor {

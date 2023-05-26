2bb3f33af1a9d1627c3b58d2c6b54017294a373e
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-05-26 09:00:17 -0500

    add comments noting what the JS interop functions do

--- jgnes-web/src/js.rs
+++ jgnes-web/src/js.rs
@@ -10,16 +10,24 @@ extern "C" {
 
 #[wasm_bindgen(module = "/js/ui.js")]
 extern "C" {
+    /// Retrieve an item from local storage. Returns None if no item exists for this key.
     pub fn loadFromLocalStorage(key: &str) -> Option<String>;
 
+    /// Set an item in local storage.
     pub fn saveToLocalStorage(key: &str, value: &str);
 
+    /// Remove the 'Loading...' text and unhide the UI.
     pub fn initComplete();
 
+    /// Un-gray the emulator display elements, and change the given button's text to the newly
+    /// configured key.
     pub fn afterInputReconfigure(buttonId: &str, buttonText: &str);
 
+    /// Focus the canvas element. Useful because the emulator can only receive inputs while the
+    /// canvas has focus, and clicking on any UI element will take focus away.
     pub fn focusCanvas();
 
+    /// Enable or disable the Download Save / Upload Save buttons.
     pub fn setSaveButtonsEnabled(enabled: bool);
 }
 
